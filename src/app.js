5263e292cbd56614d2e75a9bfa9185b4d7c956d6
Asunder <trashemail3301@gmail.com>
2025-03-01 13:46:25 -0800

    create app
    app created, can add projects, todos and dates to those projects

---
+++ src/app.js
@@ -0,0 +1,236 @@
+import { parseJSON } from 'date-fns';
+
+/**
+ * Class for creating TODO objects.
+ */
+class Todo {
+    title = "";
+    description = "";
+    dueDate = new Date();
+    priority = 0;
+    done = false;
+    /**
+     * Creates a new Todo object
+     * @param {string} title 
+     * @param {string} description 
+     * @param {Date} dueDate 
+     * @param {Number} priority 
+     */
+    constructor (title, description, dueDate, priority, done) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.done = done;
+
+    }
+
+    /**
+     * Toggles the completion state of the todo
+     */
+    toggleDone() {
+        this.done = !this.done;
+    }
+
+    /**
+     * 
+     */
+    getDone() {
+        return this.done;
+    }
+}
+
+/**
+ * Project class that stores a list of Todos
+ */
+class Project {
+    name = "";
+    todos = [];
+
+    /**
+     * Construct a new empty project
+     * @param {string} name 
+     */
+    constructor (name) {
+        this.name = name;
+    }
+
+    addTodo (title, description, dueDate, priority) {
+        this.todos.push(new Todo(title, description, dueDate, priority, false));
+    }
+
+    deleteTodo (index) {
+        this.todos.splice(index, 1);
+    }
+
+    getTodo (index) {
+        return this.todos[index];
+    }
+
+    /**
+     * Edit a todo at an index.
+     * 
+     * If a given parameter is null, no changes will be made to that value.
+     * @param {*} index 
+     * @param {*} title 
+     * @param {*} description 
+     * @param {*} dueDate 
+     * @param {*} priority 
+     */
+    editTodo (index, title, description, dueDate, priority) {
+        let todo = this.getTodo(index);
+        if (title != null) {
+            todo.title = title;
+        }
+        if (description != null) {
+            todo.description = description;
+        }
+        if (dueDate != null) {
+            todo.dueDate = dueDate;
+        }
+        if (priority != null) {
+            todo.priority = priority;
+        }
+    }
+}
+
+/**
+ * Tests whether localStorage or sessionStorage apis are available
+ * from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#feature-detecting_localstorage
+ * @param {string} type 
+ * @returns 
+ */
+function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
+  
+
+/**
+ * App class that stores and manages app logic and data. Returns data to be used for displaying the frontend.
+ */
+class App {
+    #projects = [];
+    constructor () {
+        if (this.loadFromLocalStorage()) {
+            this.saveToLocalStorage();
+        } else {
+            let inbox = new Project("Inbox");
+            inbox.addTodo("Example todo", "This is an example of a todo!", new Date(), 0);
+            this.#projects.push(inbox);
+            console.log(localStorage.getItem("projects"));
+            this.saveToLocalStorage();
+        }
+    }
+
+    loadFromLocalStorage() {
+        // Only load from localStorage if it's available and something was saved there
+        if (storageAvailable("localStorage") && localStorage.getItem("projects") != null) {
+            let projects = JSON.parse(localStorage.getItem("projects"));
+            console.log(projects.length);
+            for (let i = 0; i < projects.length; i++) {
+                if (projects[i].name != undefined) {
+                    this.addProject(projects[i].name);
+                    // let currentProject = i;
+                    console.log(projects[i].todos);
+                    for (let j = 0; j < projects[i].todos.length; j++) {
+                        let todo = projects[i].todos[j];
+                        console.log(todo.dueDate);
+                        this.addTodo(i, todo.title, todo.description, parseJSON(todo.dueDate));
+                    }
+                }
+            }
+            console.log(projects);
+            console.log(this.#projects.length)
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    saveToLocalStorage() {
+        if (storageAvailable("localStorage")) {
+            let projects = this.getProjectList();
+            for (let i = 0; i < projects.length; i++) {
+                projects[i] = this.getProjectState(i);
+            }
+            console.log(projects);
+            localStorage.setItem("projects", JSON.stringify(projects));
+            console.log(localStorage.getItem("projects"));
+        } else {
+            console.log("Local storage unavailable");
+        }
+    }
+
+    getProjectState(i) {
+        let name = this.#projects[i].name;
+        let todos = this.#projects[i].todos;
+        return {name, todos};
+    }
+
+    getProjectList() {
+        return this.#projects.map((project) => {
+            return project.name;
+        });
+    }
+
+    /**
+     * Toggle the completion status of a todo
+     * @param {int} project The index of the project
+     * @param {int} todo The index of the todo
+     */
+    toggleDone(project, todo) {
+        this.#projects[project].todos[todo].toggleDone();
+        this.saveToLocalStorage();
+    }
+
+    /**
+     * Get the completion status of a todo
+     * @param {int} project The index of the project
+     * @param {int} todo The index of the todo
+     * @returns Completion status of the todo
+     */
+    getDone(project, todo) {
+        return this.#projects[project].todos[todo].done
+    }
+    /**
+     * Add a new project to the app
+     * @param {string} name Name of the new project
+     */
+    addProject(name) {
+        this.#projects.push(new Project(name));
+        this.saveToLocalStorage();
+        return this.#projects.length - 1;
+    }
+
+    deleteTodo(project, todo) {
+        this.#projects[project].deleteTodo(todo);
+        this.saveToLocalStorage();
+    }
+
+    editTodo(project, todoIndex, title, description, date) {
+        this.#projects[project].editTodo(todoIndex, title, description, date, null);
+        this.saveToLocalStorage();
+    }
+
+    addTodo(project, title, description, date) {
+        this.#projects[project].addTodo(title, description, date, 0);
+        this.saveToLocalStorage();
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,122 @@
+import App from "./app";
 import "./styles.css";
+import { format, parseISO } from 'date-fns';
 
-alert('Hello World');
+let currentProject = 0;
+
+
+function addProject(app) {
+    let projectName = window.prompt("What do you want your new project to be called?");
+    currentProject = app.addProject(projectName);
+    // Assume the user wants to switch to the newly created project
+    updateDisplay(app);
+}
+
+/**
+ * Generates the form for creating/editing a todo
+ * @param {App} app The app object
+ * @param {HTMLElement} todoDiv The frontend div of the todo being created/edited
+ * @param {int} todoIndex The index of the todo
+ */
+function todoForm(app, todoDiv, todoIndex) {
+    let todo = app.getProjectState(currentProject).todos[todoIndex];
+    todoDiv.textContent = "";
+    // const form = document.createElement("form");
+    const titleField = document.createElement("input");
+    titleField.type = "text";
+    titleField.placeholder = "Title";
+    titleField.value = todo.title;
+    const descriptionField = document.createElement("input");
+    descriptionField.type = "text";
+    descriptionField.placeholder = "Description";
+    descriptionField.value = todo.description;
+    const dateField = document.createElement("input");
+    dateField.type = "date";
+    dateField.value = format(todo.dueDate, "yyyy-MM-dd");
+    const submit = document.createElement("button");
+    submit.textContent = "Save";
+    submit.addEventListener("click", () => {
+        app.editTodo(currentProject, todoIndex, titleField.value, descriptionField.value, parseISO(dateField.value));
+        updateDisplay(app);
+    });
+
+    todoDiv.appendChild(titleField);
+    todoDiv.appendChild(descriptionField);
+    todoDiv.appendChild(dateField);
+    todoDiv.appendChild(submit);
+}
+
+/**
+ * Updates the DOM to reflect the app state
+ * @param {App} app 
+ */
+function updateDisplay(app) {
+    const navigationList = document.getElementById("navigation-list");
+    const todolistDiv = document.getElementById("todos");
+    navigationList.textContent = "";
+    todolistDiv.textContent = "";
+
+    let projects = app.getProjectList();
+    for (let i = 0; i < projects.length; i++) {
+        const projectTag = document.createElement("p");
+        projectTag.textContent = projects[i];
+        projectTag.addEventListener("click", (counter) => {
+            currentProject = i;
+            updateDisplay(app);
+        })
+        navigationList.appendChild(projectTag);
+    }
+    const newProject = document.createElement("button");
+    newProject.textContent = "+ Create project";
+    newProject.addEventListener("click", () => {addProject(app);})
+    navigationList.appendChild(newProject);
+
+    let projectTodos = app.getProjectState(currentProject);
+    let j = 0; 
+    for (const todo of projectTodos.todos) {
+        const todoDiv = document.createElement("div");
+        todoDiv.className = "todo";
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = app.getProjectState(currentProject).todos[j].getDone()
+        checkbox.addEventListener("input", (_event) => {
+            app.toggleDone(currentProject, j-1); // The -1 is necessary no I don't know why
+        });
+        const name = document.createElement("p");
+        name.textContent = todo.title;
+        name.addEventListener("click", () => {
+            todoForm(app, todoDiv, j-1);
+        });
+        const date = document.createElement("p");
+        date.textContent = format(todo.dueDate, "MM/dd/yyyy")
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "X";
+        deleteButton.addEventListener("click", () => {
+            app.deleteTodo(currentProject, j);
+            updateDisplay(app);
+        });
+
+        todoDiv.appendChild(checkbox);
+        todoDiv.appendChild(name);
+        todoDiv.appendChild(date);
+        todoDiv.appendChild(deleteButton);
+        todolistDiv.appendChild(todoDiv);
+        j++;
+    }
+
+    const newTask = document.createElement("button");
+    newTask.textContent = "+ Create task";
+    newTask.addEventListener("click", (event) => {
+        event.target.style.display = "none";
+        const todoDiv = document.createElement("div");
+        todoDiv.className = "todo";
+        app.addTodo(currentProject, "", "", new Date());
+        todoForm(app, todoDiv, app.getProjectState(currentProject).todos.length-1);
+        todolistDiv.appendChild(todoDiv);
+
+    })
+    todolistDiv.appendChild(newTask);
+}
+
+let app = new App();
+updateDisplay(app);
